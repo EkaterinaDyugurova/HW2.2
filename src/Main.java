5a1a13eee5b3215032de599cfe470aec09841c60
Dyugurova Ekaterina <dugurowa.k@yandex.ru>
2022-09-14 20:00:03 +0400

    program

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance1 = 100;
+        int refill = 100;
+        int balance2 = balance1 + refill;
+        int bonus;
+
+        if (refill >= 1000) {
+            bonus = balance2/100;
+        } else {
+            bonus = 0;
+        }
+        System.out.print(bonus);
+
+    }
+}
