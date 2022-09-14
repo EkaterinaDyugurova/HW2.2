208570d4801b820b6eb6015e929bb56c2794f403
Dyugurova Ekaterina <dugurowa.k@yandex.ru>
2022-09-14 20:11:57 +0400

    change

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int balance1 = 100;
-        int refill = 100;
+        int refill = 1000;
         int balance2 = balance1 + refill;
         int bonus;
-        //System.out.print(balance2);
-
+        System.out.println(balance2);
         if (refill >= 1000) {
             bonus = balance2/100;
         } else {
             bonus = 0;
         }
         System.out.print(bonus);
-
-
-
     }
 }
